de40b258569de1989f21b4f6ee2589d7bf0e926d
huhouhua <huhouhuam@outlook.com>
2025-04-21 00:05:19 +0800

    feat: implementation get single metadata

--- meta.go
+++ meta.go
@@ -137,7 +137,34 @@ type ListMetadataOptions struct {
 	IncludeUnlisted   bool
 }
 
+// ListMetadata List of package metadata.
 func (p *PackageMetadataResource) ListMetadata(id string, opt *ListMetadataOptions, options ...RequestOptionFunc) ([]*PackageSearchMetadataRegistration, *http.Response, error) {
+	return p.getMetadata(id, opt, All, options...)
+}
+
+// GetMetadata returns the registration metadata for the id and version
+func (p *PackageMetadataResource) GetMetadata(id, version string, options ...RequestOptionFunc) (*PackageSearchMetadataRegistration, *http.Response, error) {
+	opt := &ListMetadataOptions{
+		IncludePrerelease: true,
+		IncludeUnlisted:   true,
+	}
+	v, err := semver.NewVersion(version)
+	if err != nil {
+		return nil, nil, err
+	}
+	versionRange := NewVersionRange(v, v, true, true)
+	list, resp, err := p.getMetadata(id, opt, versionRange, options...)
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(list) >= 0 {
+		return list[0], resp, nil
+	}
+	return nil, nil, err
+}
+
+// getMetadata retrieves metadata for a given package ID and version range.
+func (p *PackageMetadataResource) getMetadata(id string, opt *ListMetadataOptions, versionRange *VersionRange, options ...RequestOptionFunc) ([]*PackageSearchMetadataRegistration, *http.Response, error) {
 	packageId, err := parseID(id)
 	if err != nil {
 		return nil, nil, err
@@ -154,7 +181,6 @@ func (p *PackageMetadataResource) ListMetadata(id string, opt *ListMetadataOptio
 		return nil, resp, err
 	}
 	packages := make([]*PackageSearchMetadataRegistration, 0)
-	versionRange := All
 	for _, item := range index.Items {
 		if item == nil {
 			return nil, resp, fmt.Errorf("invalid %s", baseURL.String())
@@ -167,6 +193,7 @@ func (p *PackageMetadataResource) ListMetadata(id string, opt *ListMetadataOptio
 	return packages, resp, nil
 }
 
+// addMetadataToPackages adds metadata to the given packages slice based on the provided registration page and options.
 func (p *PackageMetadataResource) addMetadataToPackages(packages *[]*PackageSearchMetadataRegistration, page *registrationPage, opt *ListMetadataOptions, versionRange *VersionRange) error {
 	Lower, err := semver.NewVersion(page.Lower)
 	if err != nil {
@@ -207,6 +234,7 @@ func (p *PackageMetadataResource) addMetadataToPackages(packages *[]*PackageSear
 	return nil
 }
 
+// configureMetadataUrl configures the metadata URLs for the given PackageSearchMetadataRegistration.
 func (p *PackageMetadataResource) configureMetadataUrl(catalogEntry *PackageSearchMetadataRegistration) error {
 	reportAbuseUrl := p.client.getResourceUrl(ReportAbuseUriTemplate)
 	detailUrl := p.client.getResourceUrl(PackageDetailsUriTemplate)
@@ -217,6 +245,7 @@ func (p *PackageMetadataResource) configureMetadataUrl(catalogEntry *PackageSear
 		WithReadmeFileUrl(readmeUrl))
 }
 
+// MetadataRegistrationFunc is a function that modifies the PackageSearchMetadataRegistration.
 type MetadataRegistrationFunc func(catalogEntry *PackageSearchMetadataRegistration) error
 
 // ApplyMetadataRegistration applies a list of MetadataRegistrationFunc to a PackageSearchMetadataRegistration.
@@ -229,6 +258,7 @@ func ApplyMetadataRegistration(page *PackageSearchMetadataRegistration, options 
 	return nil
 }
 
+// WithReportAbuseUrl sets the ReportAbuseUrl field of the PackageSearchMetadataRegistration.
 func WithReportAbuseUrl(urlTemplate *url.URL) MetadataRegistrationFunc {
 	return func(catalogEntry *PackageSearchMetadataRegistration) error {
 		if urlTemplate == nil {
@@ -250,6 +280,7 @@ func WithReportAbuseUrl(urlTemplate *url.URL) MetadataRegistrationFunc {
 	}
 }
 
+// WithPackageDetailsUrl sets the PackageDetailsUrl field of the PackageSearchMetadataRegistration.
 func WithPackageDetailsUrl(urlTemplate *url.URL) MetadataRegistrationFunc {
 	return func(catalogEntry *PackageSearchMetadataRegistration) error {
 		if urlTemplate == nil {
@@ -271,6 +302,7 @@ func WithPackageDetailsUrl(urlTemplate *url.URL) MetadataRegistrationFunc {
 	}
 }
 
+// WithReadmeFileUrl sets the ReadmeFileUrl field of the PackageSearchMetadataRegistration.
 func WithReadmeFileUrl(urlTemplate *url.URL) MetadataRegistrationFunc {
 	return func(catalogEntry *PackageSearchMetadataRegistration) error {
 		if urlTemplate == nil {
