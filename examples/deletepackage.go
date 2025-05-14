e1815e362214477e53863264cfee004ddfb0ce8a
huhouhua <huhouhuam@outlook.com>
2025-05-15 02:23:13 +0800

    docs: fix all documentation mistakes and examples

--- examples/deletepackage.go
+++ examples/deletepackage.go
@@ -1,9 +1,14 @@
+// Copyright (c) 2025 Kevin Berger <huhouhuam@outlook.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
-	"github.com/huhouhua/go-nuget"
 	"log"
 	"time"
+
+	"github.com/huhouhua/go-nuget"
 )
 
 func deletePackageExample() {
--- examples/getmetadata.go
+++ examples/getmetadata.go
@@ -1,10 +1,15 @@
+// Copyright (c) 2025 Kevin Berger <huhouhuam@outlook.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
 	"fmt"
-	"github.com/huhouhua/go-nuget"
 	"log"
 	"time"
+
+	"github.com/huhouhua/go-nuget"
 )
 
 func getMetadataExample() {
